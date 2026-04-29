596c97e850790a30e035f7636f20711b39067663
olittleharmony <olittleharmony@gmail.com>
2026-04-29 09:51:20 +0700

    Add files via upload

---
+++ lho-attendance/js/config.js
@@ -0,0 +1,9 @@
+const APP_CONFIG = {
+  SUPABASE_URL:      'https://tovitqcgmsmohbjmaifk.supabase.co',
+  SUPABASE_ANON_KEY: 'GANTI_DENGAN_ANON_KEY',
+  GAS_URL:           'GANTI_DENGAN_GAS_URL',
+  APP_NAME:          'LHO Absensi',
+  ORG_NAME:          'Little Harmony Orchestra',
+  APP_URL:           'https://olittleharmony.github.io/lho-attendance',
+};
+Object.freeze(APP_CONFIG);
